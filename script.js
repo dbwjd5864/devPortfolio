cdb1f42dcf20ded1aef022e43abf95d5c37d7a01
Yujeong Choung <yjchoung10@gmail.com>
2021-02-28 00:23:34 -0800

    html,src, css added

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-const openMenu = document.getElementById("menu-open");
-const closeMenu = document.getElementById("menu-close");
-const menuList = document.getElementById("menu-list");
-const navLinks = document.querySelectorAll(".nav-link");
+const openMenu = document.getElementById('menu_open');
+const closeMenu = document.getElementById('menu_close');
+const menuList = document.getElementById('menu_list');
+const navLinks = document.querySelectorAll('.nav_link');
+
+const workTitle = document.querySelectorAll('#work_spell');
+const bouncingBalls = document.querySelectorAll('#bouncing_ball');
 
 // Open the Menu
-openMenu.addEventListener("click", () => {
-  menuList.classList.add("active");
+openMenu.addEventListener('click', () => {
+  menuList.classList.add('active');
 });
 
 // Close the Menu
-closeMenu.addEventListener("click", () => {
-  menuList.classList.remove("active");
+closeMenu.addEventListener('click', () => {
+  menuList.classList.remove('active');
 });
 
 // Close the Menu after clicking the links
 navLinks.forEach((link) => {
-  link.addEventListener("click", navAction);
+  link.addEventListener('click', navAction);
 });
 
 function navAction() {
   navLinks.forEach((link) => {
-    link.classList.remove("current");
-    menuList.classList.remove("active");
+    link.classList.remove('current');
+    menuList.classList.remove('active');
   });
-  this.classList.add("current");
+  this.classList.add('current');
 }
 
-// Move the circle above the word "Work"
+// Scroll to specific position
+
+const scrollToY = (y) => {
+  window.scrollTo({
+    top: y,
+    behavior: 'smooth',
+  });
+};
